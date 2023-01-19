a63938af1a5baff0f1a6ce2c7b07f7f2e83af2ab
Marian Zburlea <marian@wowdop.com>
2023-01-19 09:37:31 +0000

    test styled components remix 1.11.0

---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  border: 1px solid red;
+`
+
+export default function Index() {
+  return (
+    <div>
+      <H1>test</H1>
+    </div>
+  )
+}
