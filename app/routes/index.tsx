ff607d9b8a02fb09ab762a81aa66eb1773a62dc4
Marian Zburlea <marian@wowdop.com>
2023-01-19 09:54:25 +0000

    feat: add box

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,10 +5,23 @@ const H1 = styled.h1`
   border-radius: 2rem;
 `
 
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+`
+
 export default function Index() {
   return (
-    <div>
-      <H1>test</H1>
-    </div>
+    <Box>
+      <H1>test 1</H1>
+      <H1>test 2</H1>
+      <H1>test 3</H1>
+      <H1>test 4</H1>
+      <H1>test 5</H1>
+      <H1>test 6</H1>
+      <H1>test 7</H1>
+    </Box>
   )
 }
